27f00501d8d196e06c18c926a1ec36b90c20011c
Doug Baumwall <dougba@users.noreply.github.com>
2025-01-21 12:54:21 -0500

    Updated/Added simply-static-1-1737481581

--- oncehub-contact-form.js
+++ oncehub-contact-form.js
@@ -1,16 +1,4 @@
 // FormOnce embed START
-(function() {
-    var foScript = document.createElement('script');
-    foScript.id = 'fo-index';
-    foScript.src = 'https://cdn.oncehub.com/fo/form.js';
-    foScript.setAttribute('data-fo-params', 'website_id=WEB-5DEADA5CF1');
-    document.head.appendChild(foScript);
-})();
-
-document.addEventListener("DOMContentLoaded", function() {
-    var formDiv = document.createElement('div');
-    formDiv.setAttribute('data-fo-form', 'FORM-A66A47C87D');
-    formDiv.className = 'oncehub-contact';
-    document.body.appendChild(formDiv);
-});
+document.write('<script id="fo-index" src="https://cdn.oncehub.com/fo/form.js" data-fo-params="website_id=WEB-5DEADA5CF1"></script>');
+document.write('<div data-fo-form="FORM-A66A47C87D" class="oncehub-contact"></div>');
 // FormOnce embed END
