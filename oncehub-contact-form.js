25d13305c14aa874c8f87d4e4da2576df730e030
Doug Baumwall <dougba@users.noreply.github.com>
2026-07-20 16:52:52 -0400

    Remove FormOnce embed code from contact form

--- oncehub-contact-form.js
+++ oncehub-contact-form.js
@@ -1,4 +1,1 @@
-// FormOnce embed START
-document.write('<script id="fo-index" src="https://cdn.oncehub.com/fo/form.js" data-fo-params="website_id=WEB-5DEADA5CF1"></script>');
-document.write('<div data-fo-form="FORM-A66A47C87D" class="oncehub-contact"></div>');
-// FormOnce embed END
+
